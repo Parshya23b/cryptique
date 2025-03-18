721c6ad088f4ff178eaf51d419de33fcfadd2740
ritik2974 <ritik2974@gmail.com>
2025-03-18 14:06:46 +0530

    login signup

---
+++ vite/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Interface from './Interface.jsx'
+import './App.css';
+function App() {
+  return (
+    <>
+    <Interface></Interface>
+    </>
+  )
+}
+
+export default App
